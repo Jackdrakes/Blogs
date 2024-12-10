25dab76cf3e3ddee00c46c8d66a90785fd52ba13
jackdrakes <saran332003@gmail.com>
2024-12-10 09:13:03 +0530

    UPDATE: homepage
    In home page , update mobile ui

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -20,20 +20,23 @@ export default function BlogCard({ post }: BlogCardProps) {
 
   return (
     <Link href={`/post/${post.slug}`}>
-      <div className="rounded-lg bg-gray-800 overflow-hidden hover:ring-2 hover:ring-blue-500 transition">
+      <div className="lg:rounded-lg  bg-gray-800 overflow-hidden hover:ring-2 hover:ring-blue-500 transition">
         {firstImage && (
-          <div className="relative h-48">
+          <div className="relative h-48 md:h-50">
             <Image
               src={firstImage}
               alt={post.title}
               fill
               className="object-cover"
             />
+
+            <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black via-transparent to-transparent">
+              <h2 className="text-xl font-semibold text-white truncate">{post.title}</h2>
+            </div>
           </div>
+          
         )}
-        <div className="p-4">
-          <h2 className="text-xl font-semibold">{post.title}</h2>
-        </div>
+        
       </div>
     </Link>
   )
